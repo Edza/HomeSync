a8c6271b6b1cb0698e99078f0ec2c0767a11e12f
edza <edza@LIELAISPUMPIS>
2016-06-30 01:06:36 +0300

    Add project files.

---
+++ HomeSync/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HomeSync
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Log("Program started.");
+        }
+
+        private void Log(string s)
+        {
+            this.eventLog.Items.Add(DateTime.Now.ToString("hh:mm:ss") + " : " + s);
+        }
+    }
+}
